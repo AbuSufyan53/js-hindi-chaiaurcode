fb6ace014b45db4acc534fad480785ba6de4170f
Abu Sufyan <121228482+AbuSufyan53@users.noreply.github.com>
2023-12-06 08:55:51 +0000

    typeof revision

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,11 @@
 console.log("js")
+let bignum = 873354389395478422343432324324324324232n
+console.log(typeof bignum)
+let val = null
+console.log(typeof val)
+
+let un = undefined
+console.log(typeof un)
+
+let sym = Symbol("123")
+console.log(typeof sym)
